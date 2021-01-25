db39e7cdbb916b65ce6935e478a741fb83a16108
Levon Vardevanyan <levon.vardevanyan@solarwinds.com>
2021-01-25 16:40:51 +0300

    First idea: remove the code duplication

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ var button = document.querySelector('button');
 var player1 = document.querySelector('.img1');
 var player2 = document.querySelector('.img2');
 var textMessage = document.querySelector('.text_message');
-var player1Score;
-var player2Score;
-
 
 button.addEventListener('click', function(){
 	button.textContent = "Try again!";
-	randImg1();
-	randImg2();
+	var player1Score = generateScore(player1);
+	var player2Score = generateScore(player2);
 	if(player1Score > player2Score){
 		textMessage.textContent = "Player 1 wins. Congratulations!";
 	} else if(player2Score > player1Score){
@@ -19,18 +16,9 @@ button.addEventListener('click', function(){
 	}
 });
 
-function randImg1(){
-	var img1Generation = Math.floor((Math.random() * 6) + 1);
-	var standartSrc = "images/dice" + img1Generation + ".png"
-	player1.src = standartSrc;
-	player1Score = img1Generation;
+function generateScore(player) {
+	var randomScore = Math.floor((Math.random() * 6) + 1);
+	var standartSrc = "images/dice" + randomScore + ".png"
+	player.src = standartSrc;
+	return randomScore
 }
-
-function randImg2(){
-	var img2Generation = Math.floor((Math.random() * 6) + 1);
-	var standartSrc = "images/dice" + img2Generation + ".png"
-	player2.src = standartSrc;
-	player2Score = img2Generation;
-}
-
-
