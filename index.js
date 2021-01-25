936646ffc3cac2feb74c61fb3f144df75864df92
Levon Vardevanyan <levon.vardevanyan@solarwinds.com>
2021-01-25 16:41:07 +0300

    Possible alternative, small duplication though

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ var player1 = document.querySelector('.img1');
 var player2 = document.querySelector('.img2');
 var textMessage = document.querySelector('.text_message');
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function() {
 	button.textContent = "Try again!";
-	var player1Score = generateScore(player1);
-	var player2Score = generateScore(player2);
-	if(player1Score > player2Score){
+	var player1Score = rollTheDice()
+	player1.src = scoreToImageSrc(player1Score)
+
+	var player2Score = rollTheDice()
+	player2.src = scoreToImageSrc(player2Score)
+
+	if (player1Score > player2Score) {
 		textMessage.textContent = "Player 1 wins. Congratulations!";
-	} else if(player2Score > player1Score){
+	} else if(player2Score > player1Score) {
 		textMessage.textContent = "Player 2 wins. Congratulations!";
 	} else {
 		textMessage.textContent = "This is a draw =/ Try again.";
 	}
 });
 
-function generateScore(player) {
-	var randomScore = Math.floor((Math.random() * 6) + 1);
-	var standartSrc = "images/dice" + randomScore + ".png"
-	player.src = standartSrc;
-	return randomScore
+function rollTheDice() {
+	return Math.floor((Math.random() * 6) + 1);
+}
+
+function scoreToImageSrc(score) {
+	return "images/dice" + score + ".png"
 }
